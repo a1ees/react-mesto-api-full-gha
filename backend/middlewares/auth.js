e5b77d04c3180630d13adc982f7b4f36db11a5be
alees <nprolol8@gmail.com>
2023-10-31 20:31:23 +0300

    fixed jwt

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,6 +1,8 @@
 const jwt = require('jsonwebtoken');
 const UnathorizedError = require('../errors/unathorized-err');
 
+const { NODE_ENV, JWT_SECRET } = process.env;
+
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
   if (!token) {
@@ -10,7 +12,7 @@ module.exports = (req, res, next) => {
 
   let payload;
   try {
-    payload = jwt.verify(token, 'some-secret-key');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     next(new UnathorizedError('Неверный токен, необходима авторизация'));
     return;
