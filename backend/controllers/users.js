832c184e8164ebd2223e5638aafd35f15866b7c9
alees <nprolol8@gmail.com>
2024-02-08 23:49:58 +0300

    fix: adjust cookies for cross-domain access

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -23,7 +23,7 @@ module.exports.login = async (req, res, next) => {
     }
 
     const token = await jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
-    res.cookie('jwt', token, { maxAge: 3600000 * 7, httpOnly: true, sameSite: 'None' });
+    res.cookie('jwt', token, { maxAge: 3600000 * 7, httpOnly: true, sameSite: 'None', secure: true });
     res.send({ _id: user._id });
   } catch (error) {
     next(error);
